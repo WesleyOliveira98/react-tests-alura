33b11e5cf9d73f4438a5a39df74d226672effbf5
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-01-15 22:56:53 -0300

    Começando a escrever testes

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import App from './App';
+
+describe('Componente principal', () => {
+    describe('Quando eu abro o app do banco', () => {
+        test('o nome é exibido', () => {
+            render(<App />);
+            expect(screen.getByText('ByteBank')).toBeInTheDocument();
+        })
+        it('o saldo é exibido', () => {
+            render(<App />);
+            expect(screen.getByText('Saldo:')).toBeInTheDocument();
+        })
+        it('o botão de realizar transação é exibido', () => {
+            render(<App />);
+            expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+        })
+    })
+})
