f11986e16f24210925062740b09c901efe134d08
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-01-15 23:11:27 -0300

    1. Começando a escrever testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
-import App from './App';
+import App, { calcularNovoSaldo } from './App';
 
 describe('Componente principal', () => {
     describe('Quando eu abro o app do banco', () => {
-        test('o nome é exibido', () => {
+        it('o nome é exibido', () => {
             render(<App />);
             expect(screen.getByText('ByteBank')).toBeInTheDocument();
         })
